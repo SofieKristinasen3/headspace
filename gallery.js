82cd071aa94b25d4b3bd68581c1e5113ee74c3dc
CamillaFrederiks1 <cammfrederiksen@gmail.com>
2024-10-22 11:24:33 +0200

    galleri slideshow

--- gallery.js
+++ gallery.js
@@ -1,2 +1,45 @@
-let imageArray = new Array();
-imageArray = 'img/hs-buckethat-girls.png';
+var images = [
+    'img/hs-two-girls.jpg',
+    'img/hs-buckethat-girls.jpg',
+    'img/hs-group-outside3.jpg',
+    
+
+  ];
+  
+  var index = 0;
+  
+  var the_image_left = document.getElementById("left-image");
+  var the_image = document.getElementById("main-image");
+  var the_image_right = document.getElementById("right-image");
+  
+  the_image_left.src = images[4];
+  the_image.src = images[0];
+  the_image_right.src = images[1];
+
+  
+  function show_image(direction)
+  {
+    if (direction == "left")
+    {
+      index--;
+    }
+    else
+    {
+      index++;
+      index %= images.length;
+    }
+    
+    if (index < 0)
+    {
+      index = images.length - 1;
+    }
+    
+    the_image.src = images[index];
+    
+      var left_index = (index - 1 + images.length) % images.length;
+    the_image_left.src = images[left_index];
+    
+    var right_index = (index + 1) % images.length;
+    the_image_right.src = images[right_index];
+  }
+  
