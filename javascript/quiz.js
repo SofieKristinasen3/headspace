72f93be08887e934bf11ac1200bf61848d4d03ec
SofieKristinasen3 <sokr62006@edu.ucl.dk>
2024-10-27 13:13:07 +0100

    quiz rettelser + notationer

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,32 +1,31 @@
-//Array med spørgsmål, samt array indeni for de forskellige svarmuligheder. 
 
 // Quiz funktion, Sofie
 
-function startQuiz() { 
-	document.getElementById("hero_cover").style.display="none"; 
-	document.getElementById("quiz").style.display="block"; 
-}
+// function startQuiz() { 
+// 	document.getElementById("hero_cover").style.display="none"; 
+// 	document.getElementById("quiz").style.display="block"; 
+// }
 
 const quizData = [
     {
       question: "Hvor meget tid har du lyst til at bruge?",
-      answers: [{text: "2-4 timer om måneden", value: 0},{text:"4-6 timer om ugen", value: 1}, {text:"En enkelt dag", value: 2}, {text:"4-8 timer om måneden", value: 3}],
+      answers: [{text: "2-4 timer om måneden", category: 0},{text:"4-6 timer om ugen", category: 1}, {text:"En enkelt dag", category: 2}, {text:"4-8 timer om måneden", category: 3}],
     },
     {
       question: "Hvad er din motivation for at hjælpe andre?",
-      answers: [{text: "At opbygge nye relationer med hvor du allerede findes", value: 0},{text:"At hjælpe den individuelle person", value: 1} ,{text:"At have det sjovt samtidig med at jeg ved det gør en forskel", value: 2}, {text:"At tage ansvar og skabe konkrete løsninger for et breder publikum", value: 3}],
+      answers: [{text: "At opbygge nye relationer med hvor du allerede findes", category: 0},{text:"At hjælpe den individuelle person", category: 1} ,{text:"At have det sjovt samtidig med at jeg ved det gør en forskel", category: 2}, {text:"At tage ansvar og skabe konkrete løsninger for et breder publikum", category: 3}],
     },
     {
       question: "Hvad håber du at få ud af din tid hos headspace?",
-      answers: [{text: "Styrke min kreativitet og finde på nye idéer til online content", value: 0},{text:"At blive bedre til at lytte og bruge min empati ", value: 1} ,{text:"At holde mange bolde i luften og opbygge en stemning ", value: 2}, {text:"At præsentere foran mange mennesker og formidle et budskab", value: 3}],
+      answers: [{text: "Styrke min kreativitet og finde på nye idéer til online content", category: 0},{text:"At blive bedre til at lytte og bruge min empati ", category: 1} ,{text:"At holde mange bolde i luften og opbygge en stemning ", category: 2}, {text:"At præsentere foran mange mennesker og formidle et budskab", category: 3}],
     },
     {
       question: "Hvordan har du det med at tale med andre?",
-      answers: [{text: "Jeg foretrækker at arbejde i mindre teams", value: 0},{text:"Det kommer naturligt for mig, og jeg kan godt lide at fordybe mig i samtaler", value: 1} ,{text:"Jeg kan godt lide at tale med mange forskellige mennesker på en aften", value: 2}, {text:"Jeg kan godt lide at tale foran mange mennesker og præsentere foran andre", value: 3}],
+      answers: [{text: "Jeg foretrækker at arbejde i mindre teams", category: 0},{text:"Det kommer naturligt for mig, og jeg kan godt lide at fordybe mig i samtaler", category: 1} ,{text:"Jeg kan godt lide at tale med mange forskellige mennesker på en aften", category: 2}, {text:"Jeg kan godt lide at tale foran mange mennesker og præsentere foran andre", category: 3}],
     },
     {
       question: "Hvad gør dig mest glad?",
-      answers: [{text: "At finde på nye idéer og være kreativ", value: 0},{text:"At tale med andre om følelser og oplevelser", value: 1} ,{text:"At være med til større events og være en del af stemningen", value: 2}, {text:"At fortælle om et emne jeg er passioneret omkring", value: 3}],
+      answers: [{text: "At finde på nye idéer og være kreativ", category: 0},{text:"At tale med andre om følelser og oplevelser", category: 1} ,{text:"At være med til større events og være en del af stemningen", category: 2}, {text:"At fortælle om et emne jeg er passioneret omkring", category: 3}],
     },
   ];
   const quizResults = [
@@ -49,7 +48,7 @@ const quizData = [
     let questionCard = quizData[currentQuestion];
     questionElement.innerText = questionCard.question;
   
-    //Indsætter svarmuligheder, [] fjener tidligere svarmuligheder for erstatte med de nye
+    //Indsætter svarmuligheder, [] fjener tidligere svarmuligheder for erstatte med de nye, ved at indsætte et tomt array
     optionsElement.innerHTML = [];
 
     //skaber de enkelte knapper for hver svarmulighed 
@@ -63,26 +62,29 @@ const quizData = [
     });
   }
   
-//onAswerSelected = Eventhandler, hvad der sker ved event 
-  function onAnswerSelected(e) {
+/*onAswerSelected = Eventhandler, hvad der sker ved event 
+eventarguments indeholder relevant information omkring en event, 
+såsom hvilken button der er blevet trykket på i dette tilfælde.*/
+
+  function onAnswerSelected(eventarguments) {
     let questionCard = quizData[currentQuestion];
     //tager fat i den knap der er trykket på
-    const selectedButton = e.target;
-    const answerText = selectedButton.innerText;
+    const selectedButton = eventarguments.target;
+    const buttonText = selectedButton.innerText;
   
-
-  for(let i=0; i<questionCard.answers.length; ++i){
-    if (questionCard.answers[i].text===answerText){
-      scores[questionCard.answers[i].value]++;
+    for(let i=0; i<questionCard.answers.length; ++i) {
+      let answer = questionCard.answers[i];
+      if (answer.text===buttonText) {
+        scores[answer.category]++;
+      }
     }
-  }
-  currentQuestion++;
+    currentQuestion++;
 
   //let i=0;
 
   // while(i<questionCard.answers.length){
   //   if (questionCard.answers[i].text===answerText){
-  //     scores[questionCard.answers[i].value]++;
+  //     scores[questionCard.answers[i].category]++;
   //   }
   //   ++i;
   // }
@@ -94,21 +96,30 @@ const quizData = [
     }
   }
   
+  //score bliver talt om, samt hvilken plads der er størst. 
   function showResult() {
-    let max = 0;
-    let maxIndex = -1;
-    scores.forEach((value, index) =>{
-      if(value>max){
-        max=value;
-        maxIndex = index;
+    let maxScore = 0;
+    let maxCategory = 0;
+    scores.forEach((score, category) => {
+      if(score>maxScore) {
+        maxScore = score;
+        maxCategory = category;
       }
     });
 
+    //quizzen indre HTML bliver erstattet med resultatet. $ lader js blive indsat i html 
     quiz.innerHTML = 
     `
-      <p class="quiz_result"> <b>Du passer godt som</b> ${quizResults[maxIndex]} </p>
-      <button id="openModalBtn">Bliv Frivillig!</button>
+      <p class="quiz_result"> <b>Du passer godt som</b> ${quizResults[maxCategory]} </p>
+      <button id="cta_button">Bliv Frivillig!</button>
     `;
+      
+    //CTA knap i slutningen af quizzen kører Jacques javascript under tilmeldingsformular.js 
+      let button = document.getElementById("cta_button"); 
+      button.onclick = function() {
+        modal.style.display = "block";
+        populateDropdown(); // Sørg for at drop-down bliver fyldt
+    }
   }
   
   showQuestion();
